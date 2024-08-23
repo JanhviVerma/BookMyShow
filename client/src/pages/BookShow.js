f86ba5f9e2b49cbc1965111b83a59b8827a800ca
JanhviVerma <janhvi.verma2509@gmail.com>
2024-08-23 07:10:21 +0000

    Trying to implement payment property

---
+++ client/src/pages/BookShow.js
@@ -0,0 +1,136 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { hideLoading, showLoading } from "../redux/loaderSlice";
+import { getShowById } from "../calls/shows";
+import { useNavigate, useParams } from "react-router-dom";
+import { message, Card, Row, Col, Button } from "antd";
+import moment from "moment";
+import { bookShow, makePayment } from "../calls/bookings";
+import StripeCheckout from "react-stripe-checkout";
+
+const BookShow = () => {
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const [show, setShow] = useState();
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const params = useParams();
+  const navigate = useNavigate();
+  const getData = async () => {
+    try {
+      dispatch(showLoading());
+      const response = await getShowById({ showId: params.id });
+      if (response.success) {
+        setShow(response.data);
+        // message.success(response.message);
+        console.log(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      message.error(err.message);
+      dispatch(hideLoading());
+    }
+  };
+
+  
+  const book = async (transactionId) => {
+    try {
+      dispatch(showLoading());
+      const response = await bookShow({
+        show: params.id,
+        transactionId,
+        seats: selectedSeats,
+        user: user._id,
+      });
+      if (response.success) {
+        message.success("Show Booking done!");
+        navigate("/profile");
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      message.error(err.message);
+      dispatch(hideLoading());
+    }
+  };
+
+  const onToken = async (token) => {
+    try {
+      dispatch(showLoading());
+      const response = await makePayment(
+        token,
+        selectedSeats.length * show.ticketPrice * 100
+      );
+      if (response.success) {
+        message.success(response.message);
+        book(response.data);
+         console.log(response);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(hideLoading());
+    } catch (err) {
+      message.error(err.message);
+      dispatch(hideLoading());
+    }
+  };
+
+  return (
+    <>
+      {show && (
+        <Row gutter={24}>
+          <Col span={24}>
+            <Card
+              title={
+                <div className="movie-title-details">
+                  <h1>{show.movie.title}</h1>
+                  <p>
+                    Theatre: {show.theatre.name}, {show.theatre.address}
+                  </p>
+                </div>
+              }
+              extra={
+                <div className="show-name py-3">
+                  <h3>
+                    <span>Show Name:</span> {show.name}
+                  </h3>
+                  <h3>
+                    <span>Date & Time: </span>
+                    {moment(show.date).format("MMM Do YYYY")} at{" "}
+                    {moment(show.time, "HH:mm").format("hh:mm A")}
+                  </h3>
+                  <h3>
+                    <span>Ticket Price:</span> Rs. {show.ticketPrice}/-
+                  </h3>
+                  
+                </div>
+              }
+              style={{ width: "100%" }}
+            >
+              {getSeats()}
+
+              {selectedSeats.length > 0 && (
+                <StripeCheckout
+                  token={onToken}
+                  amount={selectedSeats.length * show.ticketPrice*100}
+            
+        
+                  stripeKey="pk_test_51JKPQWSJULHQ0FL7VOkMrOMFh0AHMoCFit29EgNlVRSvFkDxSoIuY771mqGczvd6bdTHU1EkhJpojOflzoIFGmj300Uj4ALqXa"
+                >
+                  <div className="max-width-600 mx-auto">
+                    <Button type="primary" shape="round" size="large" block>
+                      Pay Now
+                    </Button>
+                  </div>
+                </StripeCheckout>
+              )}
+            </Card>
+          </Col>
+        </Row>
+      )}
+    </>
+  );
+};
+export default BookShow;
